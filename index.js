a69eab4b1b9da96ef7dd84f265f15f43e64dc38d
RusFeniks <kd.feniks@gmail.com>
2021-05-04 14:53:47 +0300

    Мееее

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
-const express = require('express');
-const mysql = require('mysql');
+import express from 'express';
+import mysql from 'mysql';
 
-const server = new express();
-const db_connection = mysql.createConnection({
+const server = express();
+
+/*  да-да, капитан безопасность палит пароли, теперь кто-то может взломать мой...
+    ммм... твитер? не помню, где я ещё юзаю заглушку эту. В любом случае, потом надо вынести их куда-нибудь и получать из конфига */
+const database = mysql.createPool({
     host: "localhost", port: "3306",
-    user: "admin", password: "225514"
+    user: "kasai", password: "225514"
 });
 
-db_connection.connect(err => {
-    !err && console.log("Соединение с БД установлено");
-    db_connection.end();
+// Тоже на вынос
+const db_name = "SMPlanner";
+
+
+/*  Вот знаешь, тут мне нужен совет. Я без понятия, куда мне выносить такие штуки
+    я не хочу, чтобы серверная часть была огромным полотном кода в один файл,
+    но как правильно это вынести и вызвать... есть-же какие-то стандарты?
+    или я могу просто юзать import и передавать туда объект database? */
+
+const DATABASE_INITIALIZATION_SQL = `
+    CREATE DATABASE IF NOT EXISTS '${db_name}';
+    USE '${db_name}';
+
+    CREATE TABLE IF NOT EXISTS 'notes' (
+        'id' int(11) NOT NULL auto_increment,
+        'user_id' int(11) NOT NULL,   
+        '[date]' date NOT NULL,
+        '[time]' time NOT NULL,
+        'title' varchar(255) NOT NULL default '',  
+        'message' text NOT NULL default '',
+        PRIMARY KEY  ('id')
+    );
+
+    CREATE TABLE IF NOT EXISTS 'users' (
+        'id' int(11) NOT NULL auto_increment,
+        'email' varchar(128) NOT NULL,
+        'fullname' varchar(255) NOT NULL default '',
+        'password' text NOT NULL
+        PRIMARY KEY ('id')
+    );
+`;
+
+database.query(DATABASE_INITIALIZATION_SQL, (err, response) => {
+    err && console.log(err);
+    response && console.log(response);
+    database.end()
 });
+
 
 
 /*
