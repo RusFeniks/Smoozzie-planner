6c52384f3cd1c6a8f887e0561724a3c8efcba284
RusFeniks <kd.feniks@gmail.com>
2021-06-05 09:36:55 +0300

    Доработал миграции

---
+++ migrations/sqls/20210521122525-demodata-up.sql
@@ -0,0 +1,8 @@
+INSERT INTO tips (user_id, date, time, title, message)
+VALUES (1, '2021-05-21', '12:34:00', 'Test title #1', 'Test message #1');
+
+INSERT INTO tips (user_id, date, time, title, message)
+VALUES (1, '2021-05-25', '09:00:00', 'Test title #2', 'Test message #2');
+
+INSERT INTO tips (user_id, date, time, title, message)
+VALUES (1, '2021-05-22', '22:05:00', 'Test title #3', 'Test message #3');
